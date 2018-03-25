40efb5ebd864096e1ce9a624ca88ae011ac9dcd1
Jessie Engstrom <jessielengstrom@gmail.com>
2018-03-25 15:48:28 -0700

    Added doc strings and finished insert to a linked list problem.

--- hackerrank.py
+++ hackerrank.py
@@ -1,4 +1,5 @@
 def is_matched(expression):
+    """Checks if all brackets are matching."""
     close = {')':'(', '}':'{', ']':'['}
     opens = set(close.values())
     balanced = []
@@ -17,6 +18,7 @@ def is_matched(expression):
     return balanced == []    
 
 def ransom_note(magazine, ransom):
+    """Checks if all words in ransom note are in magazine."""
     ransom_d = {}
     mag_d = {}
     for word in ransom:
@@ -34,11 +36,13 @@ def ransom_note(magazine, ransom):
     return True
 
 def array_left_rotation(a, n, k):
+    """Rotates an array left by k."""
     lst1 = a[:k]
     lst2 = a[k:]
     return lst2 + lst1
 
 def has_cycle(head):
+    """Does the linked list have a cycle."""
     seen = set()
     current = head
     while current:
@@ -51,13 +55,51 @@ def has_cycle(head):
     return False
 
 def checkBST(root):
+    """Is this a binary search tree."""
     return check(root, 0, 10000)
 
 def check(root, min, max):
+    """Is this a binary search tree."""
     if root is None:
         return True
     if root.data >= max or root.data <= min:
         return False
     return check(root.left, min, root.data) and check(root.right, root.data, max)
   
+def print_list(head):
+    """Print all contents of a linked list."""
+    if head is None:
+        pass
+
+    while head:
+        print head.data
+        head = head.next
+
+
+"""
+ Insert Node at the end of a linked list 
+ head pointer input could be None as well for empty list
+ Node is defined as
+ 
+ class Node(object):
+ 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+ 
+ return back the head of the linked list in the below method
+"""
+
+def Insert(head, data):
+    """Insert data at the end of a linked list."""
+    new = Node(data)
+    if head is None:
+        head = new
+        return head
+    current = head
+    while current.next:
+        current = current.next
+        
+    current.next = new
+    return head
   
